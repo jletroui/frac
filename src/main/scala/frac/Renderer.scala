76029eda4c871ed36d9bf2432d7890c86a4d922a
jletrouit <jletrouit@hopper.com>
2012-09-09 18:02:41 -0400

    Updated copyright

--- src/main/scala/frac/Renderer.scala
+++ src/main/scala/frac/Renderer.scala
@@ -1,7 +1,7 @@
 package frac
 
 /*
- * Copyright (C) 2011 Julien Letrouit
+ * Copyright (C) 2012 Julien Letrouit
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
